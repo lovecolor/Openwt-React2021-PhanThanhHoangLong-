602d2da231d6d3aa42fb8beb892ab1821419b259
Your Name <hoanglong0507000@gmail.co$ git config --global user.email hoanglong0507000@gmail.co>
2021-06-27 16:19:47 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,23 @@
-import logo from "./logo.svg";
-import "./App.scss";
-import AddForm from "./components/Form/AddForm";
-import ListExpense from "./components/ListExpense/ListExpense";
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import Chart from "./components/Chart/Chart";
-import SelectYear from "./components/SelectYear/SelectYear";
-import { __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED } from "react-dom/cjs/react-dom.development";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const listExpense = useSelector((state) => state.listExpense);
-  const [currentSelectedYear,setCurrentSelectedYear]=useState(listExpense.length>0?listExpense[0].date.getFullYear():null)
-  const onChangeSelectYear=(e)=>{
-    setCurrentSelectedYear(e.target.value)
-  }
-
-  
   return (
     <div className="App">
-      <AddForm setCurrentSelectedYear={setCurrentSelectedYear}></AddForm>
-      <div className="content">
-        <div className="head-bar">
-          <span className="title">Filter by year</span>
-          
-          
-          <SelectYear currentSelectedYear={currentSelectedYear} setCurrentSelectedYear={setCurrentSelectedYear}></SelectYear>
-        </div>
-        <Chart currentSelectedYear={currentSelectedYear}></Chart>
-        <ListExpense currentSelectedYear={currentSelectedYear}></ListExpense>
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
