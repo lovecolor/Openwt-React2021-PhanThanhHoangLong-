100cc108b7e247183ef830a5929a685eeaca58d3
Your Name <hoanglong0507000@gmail.co$ git config --global user.email hoanglong0507000@gmail.co>
2021-07-09 14:36:43 +0700

    update

--- use-async.ts
+++ use-async.ts
@@ -1,76 +1,76 @@
 import { useCallback, useEffect, useReducer } from "react";
 
-function asyncReducer(state:{
-    loading:boolean,
-    result:any,
-    errors:string[]|null
-}, action:any) {
-    if (action.type === 'SEND') {
-      return {
-        result: null,
-        errors: null,
-        loading: true,
-      };
-    }
-  
-    if (action.type === 'SUCCESS') {
-      
-      return {
-        result: action.responseData,
-        errors: null,
-        loading: false,
-      };
-    }
-  
-    if (action.type === 'ERROR') {
-      return {
-        result: null,
-        errors: action.errorMessage,
-        loading: false,
-      };
-    }
-  
-    return state;
+function asyncReducer(state: {
+  loading: boolean,
+  result: any,
+  errors: string[] | null
+}, action: any) {
+  if (action.type === 'SEND') {
+    return {
+      result: null,
+      errors: null,
+      loading: true,
+    };
   }
 
+  if (action.type === 'SUCCESS') {
+
+    return {
+      result: action.responseData,
+      errors: null,
+      loading: false,
+    };
+  }
+
+  if (action.type === 'ERROR') {
+    return {
+      result: null,
+      errors: action.errorMessage,
+      loading: false,
+    };
+  }
+
+  return state;
+}
 
 
-const useAsync=(asyncFunction:()=>any)=>{
-    const [asyncState, dispatch]=useReducer(asyncReducer,{
-        
-        result: null,
-        errors: null,
-        loading:  false,
-      })
-      const sendRequest = useCallback(
-        async function () {
-          dispatch({ type: 'SEND' });
-          try {
-    
-            const responseData = await asyncFunction();
-            
-            
-           
-            
-            dispatch({ type: 'SUCCESS', responseData });
-          } catch (error) {
-    
-            
-            dispatch({
-              type: 'ERROR',
-              
-              errorMessage: [...(error.message || 'Something went wrong!')],
-            });
-          }
-        },
-        [asyncFunction]
-      );
-      useEffect(()=>{
-        sendRequest()
-      },[sendRequest])
-      return {
-        sendRequest,
-        ...asyncState,
-      };
+
+const useAsync = (asyncFunction: () => any) => {
+  const [asyncState, dispatch] = useReducer(asyncReducer, {
+
+    result: null,
+    errors: null,
+    loading: false,
+  })
+  const sendRequest = useCallback(
+    async function () {
+      dispatch({ type: 'SEND' });
+      try {
+
+        const responseData = await asyncFunction();
+
+
+
+
+        dispatch({ type: 'SUCCESS', responseData });
+      } catch (error) {
+
+
+        dispatch({
+          type: 'ERROR',
+
+          errorMessage: [...(error.message || 'Something went wrong!')],
+        });
+      }
+    },
+    [asyncFunction]
+  );
+  useEffect(() => {
+    sendRequest()
+  }, [sendRequest])
+  return {
+    sendRequest,
+    ...asyncState,
+  };
 }
 export default useAsync
