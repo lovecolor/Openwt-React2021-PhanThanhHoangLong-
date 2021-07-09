fd16d69fc145ff1b8c970504e692a8f26bd4809e
Your Name <hoanglong0507000@gmail.co$ git config --global user.email hoanglong0507000@gmail.co>
2021-07-09 14:26:46 +0700

    first commit

--- src/pages/AuthPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import styled from 'styled-components'
-import AuthForm from '../components/Auth/AuthForm'
-
-export default function AuthPage() {
-    return (
-        <AuthForm></AuthForm>
-    )
-}
